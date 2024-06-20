d0d9680d881e9cde97b2cc5118f9c7deb9626411
Ash <ashloraptor@gmail.com>
2024-06-20 16:00:53 -0400

    Set up framework for Tawodi Games site

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import './Footer.css';
+
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Tawodi Games </p>
+      
+      </footer>
+  )
+}
+
+export default Footer;
